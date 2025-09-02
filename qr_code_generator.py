e0b385631f350d539f8eb50879adf0770e1902ee
madsljensen <mads_lykke@hotmail.com>
2025-09-02 12:15:05 +0200

    Updated qr generator and auto  generation on commit

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -5,11 +5,11 @@ link_start = 'https://madsljensen.github.io/qr_redirects/'
 def generate_codes():
     # List subdirectories
     subdirectories = [d for d in os.listdir('.') if os.path.isdir(d) and not d.startswith('.')]
+    subdirs_no_qr = [d for d in subdirectories if not os.path.exists(os.path.join(d,f"{d}_qr.png"))]
     print("Subdirectories:", subdirectories)
-    for subdir in subdirectories:
+    for subdir in subdirs_no_qr:
         img = qrcode.make(link_start + subdir)
         img.save(os.path.join(subdir,f"{subdir}_qr.png"))
 
-if __name__ == "__main__":
-    generate_codes()
+generate_codes()
 
